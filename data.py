e94059538758243281dac6cd5d1dfac60e566917
Khushi Gupta <65602088+khushig712@users.noreply.github.com>
2020-05-20 14:48:09 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,11 +1,8 @@
-  
+## generates a list of [anchor, positive, negative] ##
 import numpy as np
 
 def triplet_generator():
-    ''' Dummy triplet generator for API usage demo only.
-    Will be replaced by a version that uses real image data later.
-    :return: a batch of (anchor, positive, negative) triplets
-    '''
+  
     while True:
         a_batch = np.random.rand(4, 96, 96, 3)
         p_batch = np.random.rand(4, 96, 96, 3)
